6fc6c11b19a7ae01d96cd3d332345cacbc69005b
CCRA <ccr22@cm3-1-01-135.managed.city.ac.uk>
2019-06-03 16:09:27 +0100

    Update ConvertToMat.m
    most recent version from Ananda

--- CODE/ConvertToMat.m
+++ CODE/ConvertToMat.m
@@ -1,53 +1,57 @@
+
+clc
 clear all;
 close all;
 
 %%
-%baseDir = 'C:\Users\aczf102\Documents\MATLAB\Data\Patients';
-baseDir     = 'D:\OneDrive - City, University of London\Acad\Research\Exeter_Fracture\DICOM';
-PatientsDir = strcat(baseDir,filesep,'Patients',filesep);
+baseDir = 'C:\Users\aczf102\Documents\MATLAB\Data';
+% baseDir     = 'D:\OneDrive - City, University of London\Acad\Research\Exeter_Fracture\DICOM';
+ChooseDir = input('Please choose folder to proceed, 0 for Normal, 1 for Patient');
 
-% read the first folder, use "T" as the first name is always TRANCHE 
-dir0    = dir(strcat(PatientsDir,'T*'));
-% It is good practice to use longer names than just i,t,j,etc.
+if ChooseDir == 0
+    PatientsDir = strcat(baseDir,filesep,'Normals',filesep);
+    dir0    = dir(strcat(PatientsDir,'N*'));
+else
+    PatientsDir = strcat(baseDir,filesep,'Patients',filesep);
+    % read the first folder, use "T" as the first name is always TRANCHE
+    dir0    = dir(strcat(PatientsDir,'T*'));
+end
+
 numFolders_0 = size(dir0,1);
 
 %%
 
-%
-% It is not good idea to use either i,j as variables as these can refer to
-% imaginary numbers, better to use names like counter_0
 for counter_0=1:numFolders_0
     % loop over folder like PAT1
     disp(strcat('----',PatientsDir,dir0(counter_0).name,'----'))
     dir1                    = dir(strcat(PatientsDir,dir0(counter_0).name,filesep,'P*'));
     numFolders_1            = size(dir1,1);
     for counter_1=1:numFolders_1
-        disp(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name))
+        %disp(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name)); %show path that being read in level Pxx
+        dir2 = dir(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,'ST*'));
+        numFolders_2=size(dir2,1);
+        for counter_2=1:numFolders_2
+            disp(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name));
+            dir3=dir(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name,filesep,'SE*'));
+            numFolders_3=size(dir3,1);
+            for counter_3=1:numFolders_3
+                disp(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name,filesep,dir3(counter_3).name));
+                dir4= dir(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name,filesep,dir3(counter_3).name,filesep,'IMG*'));
+                numFolders_4=size(dir4,1);
+                for counter_4=1:numFolders_4
+                    disp(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name,filesep,dir3(counter_3).name,filesep,dir4(counter_4).name))
+                    readDicom=dicomread(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name,filesep,dir3(counter_3).name,filesep,dir4(counter_4).name));
+                    infoDicom=dicominfo(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name,filesep,dir3(counter_3).name,filesep,dir4(counter_4).name));
+                    maskDicom=zeros(size(readDicom));
+                    if ChooseDir == 0
+                        save(['C:\Users\aczf102\Documents\MATLAB\DICOM_Karen_ANDUpdate\Exp\Normals\ANON_N_' infoDicom.PatientID ],'readDicom','infoDicom','maskDicom');
+                    else
+                        save(['C:\Users\aczf102\Documents\MATLAB\DICOM_Karen_ANDUpdate\Exp\Patients\ANON_P_' infoDicom.PatientID ],'readDicom','infoDicom','maskDicom');
+                    end
+                
+                end
+            end
+        end
     end
 end
 
-%     for j=i:size(dir1)
-%         % loop over folder like STD1
-%         dir2 = dir(strcat(baseDir,filesep,dir0(1).name,filesep,'P*',filesep,'ST*'));
-%         
-%         for k=j:size(dir2)
-%             %loop over folder like SER1
-%             dir3    = dir(strcat(baseDir,filesep,dir0(1).name,filesep,'P*',filesep,'ST*',filesep,'SER*'));
-%             dataSize = size(dir3,1);
-%             for m=k:size(dir3)
-%                 %identify dicom file name in each folder
-%                 dir4=dir(strcat(baseDir,filesep,dir0(1).name,filesep,'P*',filesep,'ST*',filesep,'SER*',filesep,'I*'));
-%                 % read dicom image and create empty mask
-%                 readDicom=dicomread(dir4(m).name);
-%                 infoDicom=dicominfo(dir4(m).name);
-%                 maskDicom=zeros(size(readDicom));
-%                 %maskDicom=double.empty;  %other alternatif mask 
-%                 %disp(strcat(dir0(k).name,'/'dir1(k1).name,'/',dir2(k2).name,etc
-%             end
-%             % convert into .mat file
-%             %save(['C:\Users\aczf102\Documents\MATLAB\DICOM_Karen_ANDUpdate\Tes4_' infoDicom.PatientID '_' infoDicom.SeriesDescription],'readDicom','infoDicom','maskDicom');
-%             save('C:\Users\aczf102\Documents\MATLAB\DICOM_Karen_ANDUpdate\Tes4','readDicom','infoDicom','maskDicom');
-%             imagesc(readDicom)
-%         end
-%     end
-% end
