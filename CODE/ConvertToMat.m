e3fa181a2044a38b323fc10c0cf8f5e6e4337331
CCRA <ccr22@cm3-1-01-135.managed.city.ac.uk>
2019-06-03 16:20:54 +0100

    Update ConvertToMat.m

--- CODE/ConvertToMat.m
+++ CODE/ConvertToMat.m
@@ -4,10 +4,11 @@ clear all;
 close all;
 
 %%
-baseDir = 'C:\Users\aczf102\Documents\MATLAB\Data';
+%baseDir = 'C:\Users\aczf102\Documents\MATLAB\Data';
+baseDir     = '/Users/ccr22/OneDrive - City, University of London/Acad/Research/Exeter_Fracture/DICOM';
 % baseDir     = 'D:\OneDrive - City, University of London\Acad\Research\Exeter_Fracture\DICOM';
-ChooseDir = input('Please choose folder to proceed, 0 for Normal, 1 for Patient');
-
+%ChooseDir = input('Please choose folder to proceed, 0 for Normal, 1 for Patient');
+ChooseDir = 1;
 if ChooseDir == 0
     PatientsDir = strcat(baseDir,filesep,'Normals',filesep);
     dir0    = dir(strcat(PatientsDir,'N*'));
@@ -31,23 +32,23 @@ for counter_0=1:numFolders_0
         dir2 = dir(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,'ST*'));
         numFolders_2=size(dir2,1);
         for counter_2=1:numFolders_2
-            disp(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name));
+            %disp(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name));
             dir3=dir(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name,filesep,'SE*'));
             numFolders_3=size(dir3,1);
             for counter_3=1:numFolders_3
-                disp(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name,filesep,dir3(counter_3).name));
+                %disp(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name,filesep,dir3(counter_3).name));
                 dir4= dir(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name,filesep,dir3(counter_3).name,filesep,'IMG*'));
                 numFolders_4=size(dir4,1);
                 for counter_4=1:numFolders_4
                     disp(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name,filesep,dir3(counter_3).name,filesep,dir4(counter_4).name))
-                    readDicom=dicomread(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name,filesep,dir3(counter_3).name,filesep,dir4(counter_4).name));
-                    infoDicom=dicominfo(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name,filesep,dir3(counter_3).name,filesep,dir4(counter_4).name));
-                    maskDicom=zeros(size(readDicom));
-                    if ChooseDir == 0
-                        save(['C:\Users\aczf102\Documents\MATLAB\DICOM_Karen_ANDUpdate\Exp\Normals\ANON_N_' infoDicom.PatientID ],'readDicom','infoDicom','maskDicom');
-                    else
-                        save(['C:\Users\aczf102\Documents\MATLAB\DICOM_Karen_ANDUpdate\Exp\Patients\ANON_P_' infoDicom.PatientID ],'readDicom','infoDicom','maskDicom');
-                    end
+                    %readDicom=dicomread(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name,filesep,dir3(counter_3).name,filesep,dir4(counter_4).name));
+                    %infoDicom=dicominfo(strcat(PatientsDir,dir0(counter_0).name,filesep,dir1(counter_1).name,filesep,dir2(counter_2).name,filesep,dir3(counter_3).name,filesep,dir4(counter_4).name));
+                    %maskDicom=zeros(size(readDicom));
+                    %if ChooseDir == 0
+                    %    save(['C:\Users\aczf102\Documents\MATLAB\DICOM_Karen_ANDUpdate\Exp\Normals\ANON_N_' infoDicom.PatientID ],'readDicom','infoDicom','maskDicom');
+                    %else
+                    %    save(['C:\Users\aczf102\Documents\MATLAB\DICOM_Karen_ANDUpdate\Exp\Patients\ANON_P_' infoDicom.PatientID ],'readDicom','infoDicom','maskDicom');
+                    %end
                 
                 end
             end
