8b267d1df843eeb490cb41e2c2b96227d3a6ee23
CCRA <ccr22@lcladms-Mac-Pro.local>
2019-11-05 13:02:01 +0000

    Update displayXrayWithMask.m
    update to have 4 landmarks

--- CODE/displayXrayWithMask.m
+++ CODE/displayXrayWithMask.m
@@ -20,13 +20,16 @@ if ~exist('sizeDilate','var')
     sizeDilate = 15;
 end
 clear Xray_mask2 Xray_RGB
-    Xray_mask2 = imdilate (Xray_mask,ones(sizeDilate));    
-    Xray_RGB(:,:,1)     = (Xray_mask2==1)+(Xray_mask2~=3).*(Xray/max(Xray(:)));
-    Xray_RGB(:,:,2)     = (Xray_mask2==2)+(Xray_mask2~=3).*(Xray/max(Xray(:)));
-    Xray_RGB(:,:,3)     = (Xray_mask2==3)+(Xray_mask2~=3).*(Xray/max(Xray(:)));
-    
+    Xray_mask2 = imdilate (Xray_mask,ones(sizeDilate)); 
+    Xray2               =  (Xray/max(Xray(:))).*(Xray_mask2==0);
+%     Xray_RGB(:,:,1)     = (Xray_mask2==1)+(Xray_mask2~=3).*(Xray/max(Xray(:)));
+%     Xray_RGB(:,:,2)     = (Xray_mask2==2)+(Xray_mask2~=3).*(Xray/max(Xray(:)));
+%     Xray_RGB(:,:,3)     = (Xray_mask2==3)+(Xray_mask2~=3).*(Xray/max(Xray(:)));
+    Xray_RGB(:,:,1)     = (Xray_mask2==1)+(Xray_mask2==4)+Xray2;
+    Xray_RGB(:,:,2)     = (Xray_mask2==2)+(Xray_mask2==4)+Xray2;
+    Xray_RGB(:,:,3)     = (Xray_mask2==3)+Xray2;
 
-
+Xray_RGB(Xray_RGB>1)=1;
 imagesc(Xray_RGB)
 %axis equal
 %grid on
