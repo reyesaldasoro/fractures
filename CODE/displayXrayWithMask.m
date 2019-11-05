3fb483978cc658ce430e5637c798e9a3cc45b17b
CCRA <ccr22@lcladms-Mac-Pro.local>
2019-11-05 12:57:29 +0000

    Create displayXrayWithMask.m
    A short script to display the mask (landmarks) with the x-ray

---
+++ CODE/displayXrayWithMask.m
@@ -0,0 +1,36 @@
+
+
+
+% If points are selected, dilate and add to the mask
+if exist('LM_Y','var')
+    
+    Xray_mask(round(LM_X(1)),round(LM_Y(1)))=1;
+    Xray_mask(round(LM_X(2)),round(LM_Y(2)))=2;
+    Xray_mask(round(LM_X(3)),round(LM_Y(3)))=3;
+
+    
+% else
+%     
+%     Xray_RGB(:,:,1)     = Xray/max(Xray(:));
+%     Xray_RGB(:,:,2)     = Xray/max(Xray(:));
+%     Xray_RGB(:,:,3)     = Xray_mask+(Xray/max(Xray(:)));
+    
+end
+if ~exist('sizeDilate','var')
+    sizeDilate = 15;
+end
+clear Xray_mask2 Xray_RGB
+    Xray_mask2 = imdilate (Xray_mask,ones(sizeDilate));    
+    Xray_RGB(:,:,1)     = (Xray_mask2==1)+(Xray_mask2~=3).*(Xray/max(Xray(:)));
+    Xray_RGB(:,:,2)     = (Xray_mask2==2)+(Xray_mask2~=3).*(Xray/max(Xray(:)));
+    Xray_RGB(:,:,3)     = (Xray_mask2==3)+(Xray_mask2~=3).*(Xray/max(Xray(:)));
+    
+
+
+imagesc(Xray_RGB)
+%axis equal
+%grid on
+colormap gray
+if exist('XrayDir','var')
+title(XrayDir(k).name,'fontsize',18,'interpreter','none')
+end
