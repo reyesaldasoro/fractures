dc0ab110aec99ed0b34c4cec5ee166258fa9a4d0
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2019-12-02 13:32:08 +0000

    Update displayXrayMetricsManyFigs.m

--- CODE/displayXrayMetricsManyFigs.m
+++ CODE/displayXrayMetricsManyFigs.m
@@ -26,22 +26,22 @@ hc = subplot(1,3,3);
 imagesc(displayResults.displayResultsLunate2)
 title('(c)','fontsize',12)
 
-ha.Position = [ 0.04    0.1    0.29    0.801];
-hb.Position = [ 0.37    0.1    0.29    0.801];
-hc.Position = [ 0.7    0.1    0.29    0.801];
+ha.Position = [ 0.035    0.1    0.285    0.801];
+hb.Position = [ 0.37    0.1    0.285    0.801];
+hc.Position = [ 0.7    0.1    0.285    0.801];
 
 
 
 %% Second row, Add the results of the finger
 figure(2)
 set(gcf,'Position', [   300   550   927   280])
-hd = subplot(4,3,4);
+hd = subplot(1,3,1);
 imagesc(displayResults.displayResultsFinger.Xray2)
 title('(a)','fontsize',12)
-he = subplot(4,3,5);
+he = subplot(1,3,2);
 imagesc(displayResults.displayResultsFinger.Combined)
 title('(b)','fontsize',12)
-hf = subplot(4,3,6);
+hf = subplot(1,3,3);
 plot(displayResults.displayResultsFinger.CorticalProfile{1},'k')
 hold on
 plot(displayResults.displayResultsFinger.centValleyLoc,displayResults.displayResultsFinger.centValley,'ro','markersize',8)
@@ -55,77 +55,67 @@ axis tight
 colormap gray
 title('(c)','fontsize',12)
 
-hd.Position = [ 0.03    0.1    0.29    0.801];
-he.Position = [ 0.36    0.1    0.29    0.801];
-hf.Position = [ 0.69   0.1    0.29    0.801];
+hd.Position = [ 0.035    0.1    0.285    0.801];
+he.Position = [ 0.37    0.1    0.285    0.801];
+hf.Position = [ 0.699   0.1    0.285    0.801];
 
 %% Third row,  add the LBP Results
-hg = subplot(4,3,7)
+figure(3)
+set(gcf,'Position', [   350   500   927   280])
+
+hg = subplot(4,3,7);
 imagesc(displayResults.displayResultsLBP.Xray_out)
 % zoom in a little bit
 [rows,cols,levs]=size(displayResults.displayResultsLBP.Xray_out);
  axis(round([0.2*cols 0.8*cols 0.3*rows 0.9*rows ]))
-title('(g)','fontsize',12)
+title('(a)','fontsize',12)
 
-hh = subplot(4,3,8)
+hh = subplot(4,3,8);
 imagesc(displayResults.displayResultsLBP.PatchExtracted)
-title('(h)','fontsize',12)
+title('(b)','fontsize',12)
 
-hi = subplot(4,3,9)
+hi = subplot(4,3,9);
 bar(dataOut.LBP_Features)
 axis tight
 grid on
-title('(i)','fontsize',12)
-
+title('(c)','fontsize',12)
+hg.Position = [ 0.035    0.1    0.285    0.801];
+hh.Position = [ 0.37    0.1    0.285    0.801];
+hi.Position = [ 0.699   0.1    0.285    0.801];
+colormap gray
 %% Fourth row add the profiles
-hj = subplot(4,3,10)
+figure(4)
+set(gcf,'Position', [   400   450   927   280])
+
+hj = subplot(1,3,1);
 imagesc(displayResults.displayResultsRadial.dataOutput)
 % zoom in a little bit
 [rows,cols,levs]=size(displayResults.displayResultsRadial.dataOutput);
  axis(round([0.2*cols 0.8*cols 0.3*rows 0.9*rows ]))
-title('(j)','fontsize',12)
+title('(a)','fontsize',12)
 
-hk = subplot(4,3,11)
+hk = subplot(1,3,2);
 hold off
 plot(displayResults.displayResultsRadial.prof_radial_new1,'r')
 hold on
 plot(displayResults.displayResultsRadial.prof_radial_new2,'b')
 grid on
 axis tight
-title('(k)','fontsize',12)
+title('(b)','fontsize',12)
 
-hl = subplot(4,3,12)
+hl = subplot(1,3,3);
 hold off
 plot(displayResults.displayResultsRadial.prof2_radial_new1,'r')
 hold on
 plot(displayResults.displayResultsRadial.prof2_radial_new2,'b')
 grid on
 axis tight
-title('(l)','fontsize',12)
+title('(c)','fontsize',12)
 % Add the lines
 %h = subplot(2,4,5)
 %imagesc(displayResults.displayResultsLunate)
 
-
-
-hTit = annotation(gcf,'textbox',...
-    [0.345199568500538 0.955709957258268 0.327939580623643 0.0292207786724681],...
-    'String',displayResults.nameFile,'interpreter','none',...
-    'FitBoxToText','on','linestyle','none');
-
-allHandles.ha = ha;
-allHandles.hb = hb;
-allHandles.hc = hc;
-allHandles.hd = hd;
-allHandles.he = he;
-allHandles.hf = hf;
-allHandles.hg = hg;
-allHandles.hh = hh;
-allHandles.hi = hi;
-allHandles.hj = hj;
-allHandles.hk = hk;
-allHandles.hl = hl;
-
-
-
-%title(displayResults.nameFile,'interpreter','none')
+hj.Position = [ 0.035    0.1    0.285    0.801];
+hk.Position = [ 0.36    0.1    0.285    0.801];
+hl.Position = [ 0.69    0.1    0.285    0.801];
+%%
