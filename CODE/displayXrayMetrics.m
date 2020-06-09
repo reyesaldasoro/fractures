9d45088008cafd2043c49886009deb20cb6cb0e7
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2020-06-09 16:30:37 +0100

    Update displayXrayMetrics.m

--- CODE/displayXrayMetrics.m
+++ CODE/displayXrayMetrics.m
@@ -3,7 +3,7 @@ function allHandles = displayXrayMetrics(displayResults,dataOut)
 
 % First row, the original image, plus landmarks, rotation and lines of width
  set(gcf,'Position', [   247   68   927   880])
-ha = subplot(4,3,1)
+ha = subplot(4,3,1);
 %imagesc(displayResults.Xray)
     sizeDilate = 55;
     Xray_mask2          = imdilate (displayResults.Xray_mask,ones(sizeDilate));   
@@ -15,22 +15,22 @@ imagesc(Xray_RGB)
 title('(a)','fontsize',12)
 
 
-hb = subplot(4,3,2)
+hb = subplot(4,3,2);
 
 imagesc(displayResults.XrayR2)
 title('(b)','fontsize',12)
-hc = subplot(4,3,3)
+hc = subplot(4,3,3);
 
 imagesc(displayResults.displayResultsLunate2)
 title('(c)','fontsize',12)
 %% Second row, Add the results of the finger
-hd = subplot(4,3,4)
+hd = subplot(4,3,4);
 imagesc(displayResults.displayResultsFinger.Xray2)
 title('(d)','fontsize',12)
-he = subplot(4,3,5)
+he = subplot(4,3,5);
 imagesc(displayResults.displayResultsFinger.Combined)
 title('(e)','fontsize',12)
-hf = subplot(4,3,6)
+hf = subplot(4,3,6);
 plot(displayResults.displayResultsFinger.CorticalProfile{1},'k')
 hold on
 plot(displayResults.displayResultsFinger.centValleyLoc,displayResults.displayResultsFinger.centValley,'ro','markersize',8)
@@ -44,32 +44,32 @@ axis tight
 colormap gray
 title('(f)','fontsize',12)
 %% Third row,  add the LBP Results
-hg = subplot(4,3,7)
+hg = subplot(4,3,7);
 imagesc(displayResults.displayResultsLBP.Xray_out)
 % zoom in a little bit
 [rows,cols,levs]=size(displayResults.displayResultsLBP.Xray_out);
  axis(round([0.2*cols 0.8*cols 0.3*rows 0.9*rows ]))
 title('(g)','fontsize',12)
 
-hh = subplot(4,3,8)
+hh = subplot(4,3,8);
 imagesc(displayResults.displayResultsLBP.PatchExtracted)
 title('(h)','fontsize',12)
 
-hi = subplot(4,3,9)
+hi = subplot(4,3,9);
 bar(dataOut.LBP_Features)
 axis tight
 grid on
 title('(i)','fontsize',12)
 
 %% Fourth row add the profiles
-hj = subplot(4,3,10)
+hj = subplot(4,3,10);
 imagesc(displayResults.displayResultsRadial.dataOutput)
 % zoom in a little bit
 [rows,cols,levs]=size(displayResults.displayResultsRadial.dataOutput);
  axis(round([0.2*cols 0.8*cols 0.3*rows 0.9*rows ]))
 title('(j)','fontsize',12)
 
-hk = subplot(4,3,11)
+hk = subplot(4,3,11);
 hold off
 plot(displayResults.displayResultsRadial.prof_radial_new1,'r')
 hold on
@@ -78,7 +78,7 @@ grid on
 axis tight
 title('(k)','fontsize',12)
 
-hl = subplot(4,3,12)
+hl = subplot(4,3,12);
 hold off
 plot(displayResults.displayResultsRadial.prof2_radial_new1,'r')
 hold on
