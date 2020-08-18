73d907f4e21348d514d3b380216673935bfdf4e4
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2020-08-18 18:08:05 +0100

    Create a new display to illustrate pre-processing

--- CODE/alignXray.m
+++ CODE/alignXray.m
@@ -63,5 +63,67 @@ if displayData==1
     imagesc(XrayR)
     title(num2str(angleRot))
     colormap gray
+elseif displayData==2
+    
+    sizeFont = 13;
+    h1=subplot(231);
+    imagesc(Xray)
+    title('(a) Original','fontsize',sizeFont)
+    
+    h2=subplot(232);
+    Xray2B = zeros(size(Xray));
+    Xray2B(rr3,cc)=Xray(rr3,cc);
+    imagesc(Xray2B)
+    title('(b) Bones of forearm','fontsize',sizeFont)
+    
+    h3=subplot(233);
+        Xray2B(rr3,cc)=imdilate(Xray4,ones(7));
+    imagesc(Xray2B)
+    title('(c) Edges detected','fontsize',sizeFont)
+    
+    h4=subplot(234);
+
+    [HoughBones,HoughAngles,HoughDist]  = hough(Xray2B,'ThetaResolution',1);
+    PeaksHough                  = houghpeaks(HoughBones,5,'threshold',ceil(0.3*max(HoughBones(:))));
+    
+    lines = houghlines(Xray2B,HoughAngles,HoughDist,PeaksHough,'FillGap',5,'MinLength',27);
+    imagesc(Xray2B)
+    hold
+    for k = 1:length(lines)
+        xy = [lines(k).point1; lines(k).point2];
+        plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+    end
+    title('(d) Strongest Hough lines','fontsize',sizeFont)
+    
+%     h4=subplot(234);
+%     imagesc(HoughBones)
+%     hold
+%     plot(PeaksHough(:,2),PeaksHough(:,1),'ro','markersize',9)
+%     
+%     caxis([0 max(HoughBones(:))/2])
+    
+    h5=subplot(235);
+    imagesc(XrayR)
+    title(strcat('(e) Rotation by ',32,num2str(angleRot),32,'^o'),'fontsize',sizeFont)
+    
+    h6=subplot(236);
+    imagesc(XrayR==0)
+    title('(f) Region outside Collimator','fontsize',sizeFont)
+    
+    set(h1,'position',[   0.02    0.51    0.3    0.45]);
+    set(h2,'position',[   0.35    0.51    0.3    0.45]);
+    set(h3,'position',[   0.68    0.51    0.3    0.45]);
+    set(h4,'position',[   0.02    0.01    0.3    0.45]);
+    set(h5,'position',[   0.35    0.01    0.3    0.45]);
+    set(h6,'position',[   0.68    0.01    0.3    0.45]);
+    colormap(gray)
+    h1.XTick=[];    h1.YTick=[];
+    h2.XTick=[];    h2.YTick=[];
+    h3.XTick=[];    h3.YTick=[];
+    h4.XTick=[];    h4.YTick=[];
+    h5.XTick=[];    h5.YTick=[];
+    h6.XTick=[];    h6.YTick=[];
+
+
 end
 %%
